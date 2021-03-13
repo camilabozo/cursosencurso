bdc804160610efee871d6b16ae86d0773e4484f6
camilabozo <66281792+camilabozo@users.noreply.github.com>
2021-03-13 00:39:11 -0300

    Add files via upload

---
+++ Practica3/formularios.php
@@ -0,0 +1,11 @@
+<?php 
+
+echo "El/la empleadx ".$_POST['nombre']." tiene ".$_POST['edad']." años, y sabe hablar en ".$_POST['idiomas'].".";
+echo "<br>";
+echo $_POST['nombre']." está ".$_POST['estado'].".";
+echo "<br>";
+echo $_POST['texto'];
+echo "<br>";
+echo $_POST['cbox'];
+
+?>
